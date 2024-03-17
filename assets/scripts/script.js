35b5e20ef74d0decc6a663db510ed70c03bee00d
ALLOMOH <105246117+ALLOMOH@users.noreply.github.com>
2024-03-17 23:38:58 +0000

    uppleod

---
+++ assets/scripts/script.js
@@ -0,0 +1,12 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const mainGrid = document.querySelector('.main-grid');
+
+    menuToggle.addEventListener('click', () => {
+      mainGrid.toggleAttribute('data-big-nav-open');
+      const menuState = menuToggle.getAttribute('aria-expanded');
+      if (menuState == 'false') {
+        menuToggle.setAttribute('aria-expanded', true);
+      } else {
+        menuToggle.setAttribute('aria-expanded', false);
+      }
+});
